f921c8bc725f23db3fb5b1c34c83193bf25017c2
zenstorage <bielhenriquek.k@gmail.com>
2025-05-23 18:09:21 -0300

    fix: atualizar versão para 0.4.6.2

--- Redgifs/Redgifs embed tweaks/script.user.js
+++ Redgifs/Redgifs embed tweaks/script.user.js
@@ -10,7 +10,7 @@
 // @grant           GM.getValues
 // @grant           GM.registerMenuCommand
 // @require         https://update.greasyfork.org/scripts/526417/1534658/USToolkit.js
-// @version         0.4.6.1
+// @version         0.4.6.2
 // @run-at          document-start
 // @author          hdyzen
 // @description     tweaks redgifs embed/iframe video
